94c836b7f4e1074708178d198943f2941ef84fbf
Podmaskov <podmaskov@gmail.com>
2024-04-15 12:21:04 +0200

    font and global styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { Global, css } from "@emotion/react";
+import emotionNormalize from "emotion-normalize";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Global
+      styles={css`
+        @import url("https://fonts.googleapis.com/css2?family=Inter:wght@400..600&display=swap")
+            ${emotionNormalize} html,
+          body {
+          padding: 0;
+          margin: 0;
+          background: white;
+          min-height: 100%;
+          font-family: "Inter", sans-serif;
+          font-size: 16px;
+          font-weight: 500;
+        }
+      `}
+    />
     <App />
   </React.StrictMode>
 );
---
+++ src/styles/global.js
@@ -0,0 +1,10 @@
+export const rootStyle = {
+  color: {
+    black: "#101828",
+    grey: "#475467",
+    red: "#E44848",
+    yellow: "#FFC531",
+    lightGrey: "#F2F4F7",
+    ultraLightGrey: "##F7F7F7",
+  },
+};
